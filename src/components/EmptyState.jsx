c8c5980eba546510678a33c7e6fe3fbf3861618a
taliyameyswara <taliyameyswara@gmail.com>
2024-10-15 14:47:47 +0700

    empty state image responsive

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import EmptyImage from "../assets/home/404.png"; // Replace with your own empty state image
+import EmptyImage from "../assets/home/404.png";
 
 const EmptyState = ({ message, ctaLabel, onCtaClick }) => {
   return (
     <div className="flex flex-col items-center justify-center text-center py-10">
-      {/* Empty State Image */}
       <img
         src={EmptyImage}
         alt="No items"
-        className="md:w-[10%] w-[20%] object-contain opacity-20 filter grayscale"
+        className="md:w-[10%] w-[25%] object-contain opacity-20 filter grayscale"
         style={{ userSelect: "none", pointerEvents: "none" }}
       />
 
@@ -17,7 +16,6 @@ const EmptyState = ({ message, ctaLabel, onCtaClick }) => {
         {message || "Tidak ada item yang ditemukan"}
       </h2>
 
-      {/* Call-to-Action Button (Optional) */}
       {ctaLabel && onCtaClick && (
         <button
           className="px-6 py-2 text-white bg-brick rounded-full hover:bg-opacity-90 transition"
